79cfa9b6da30f5ff727def13531ea13d4b907d15
sheep <sheep@sheep.sheep>
2021-11-04 10:23:19 +0700

    Simple text editor working i guess

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # soe
+d
+as
+da
+sda
+
+as
+dfa
+
+sdf
+
+
+
+
+
+asdfas
+df
+asdf
+lmao
--- main.py
+++ main.py
@@ -1,157 +1,183 @@
 import curses
 import os
 from posixpath import join
+import re
 import sys
 
 class Editor:
-	def __init__(self, stdscr, file_name):
-		self.screen_x = 0
-		self.screen_y = 0
-		self.offscreen_x = 0
-		self.offscreen_y = 0 # startx, starty
-		self.total_x = self.screen_x + self.offscreen_x
-		self.total_y = self.screen_y + self.offscreen_y
-		self.RUN = True
-		self.win = stdscr
-		self.height, self.width = self.win.getmaxyx()
-		self.status_win = stdscr.subwin(1, self.width - 1, self.height - 1, 0)
-		self.status = ''
-		self.file_name = file_name
-		if os.path.exists(file_name):
-			with open(file_name, 'r') as file:
-				self.text = file.readlines()
-		else:
-			with open(file_name, 'w') as file:
-				self.text = ''
+    def __init__(self, stdscr, file_name):
+        self.screen_x = 0
+        self.screen_y = 0
+        self.offscreen_x = 0
+        self.offscreen_y = 0 
+        self.total_x = self.screen_x + self.offscreen_x
+        self.total_y = self.screen_y + self.offscreen_y
+        self.RUN = True
+        self.win = stdscr
+        self.height, self.width = self.win.getmaxyx()
+        self.status_win = stdscr.subwin(1, self.width - 1, self.height - 1, 0)
+        self.status = ''
+        self.file_name = file_name
+        self.load_file()
 
-	def main(self):
-		while self.RUN:
-			self.win.clear()
+        curses.noecho()
+        
+    def load_file(self):
+        if os.path.exists(self.file_name):
+            with open(self.file_name, 'r') as file:
+                self.text = file.read().splitlines()
+        else:
+            with open(self.file_name, 'w') as file:
+                self.text = ''
 
-			self.total_x = self.screen_x + self.offscreen_x
-			self.total_y = self.screen_y + self.offscreen_y
-			
-			for index, line in enumerate(self.text[self.offscreen_y:]):
-				if index >= self.height:
-					break
-				self.win.insstr(index, 0, line[self.offscreen_x:self.width-1])
+    def main(self):
+        while self.RUN:
+            self.win.clear()
 
-			self.win.move(self.screen_y, self.screen_x)
+            self.total_x = self.screen_x + self.offscreen_x
+            self.total_y = self.screen_y + self.offscreen_y
+            
+            for index, line in enumerate(self.text[self.offscreen_y:]):
+                if index >= self.height:
+                    break
+                self.win.insstr(index, 0, line[self.offscreen_x:self.width-1])
 
-			status = f"{self.total_x}, {self.total_y}"
-			
-			self.status_win.clear()
-			self.status_win.addstr(0, 0, status + '  ' + self.status, curses.A_REVERSE)
+            self.win.move(self.screen_y, self.screen_x)
 
-			self.status_win.refresh()
-			self.win.refresh()
+            status = f"{self.total_x}, {self.total_y}"
+            
+            self.status_win.clear()
+            self.status_win.addstr(0, 0, status + '  ' + self.status, curses.A_REVERSE)
 
-			key = self.win.getch()
+            self.status_win.refresh()
+            self.win.refresh()
 
-			if key == 17: # ctrl q
-				self.RUN = False
-			elif key == curses.KEY_LEFT:
-				self.left()
-			elif key == curses.KEY_RIGHT:
-				self.right()
-			elif key == curses.KEY_UP:
-				self.up()
-			elif key == curses.KEY_DOWN:
-				self.down()
-			elif key == curses.KEY_BACKSPACE :
-				self.delch()
-			elif key == 19:
-				self.save()
-			elif key == curses.KEY_ENTER:
-				self.newline()
-			else:
-				self.addch(chr(key))
+            key = self.win.getch()
 
-	def newline(self):
-		self.text.insert(self.total_y + 1, '')
-		# self.text[self.total_y] = self.text[self.total_y][:self.total_x]
-		self.down()
+            if key == 17: # ctrl q
+                self.RUN = False
+            elif key == curses.KEY_LEFT:
+                self.left()
+            elif key == curses.KEY_RIGHT:
+                self.right()
+            elif key == curses.KEY_UP:
+                self.up()
+            elif key == curses.KEY_DOWN:
+                self.down()
+            elif key == curses.KEY_BACKSPACE or key == 127:  
+                self.delch()
+            elif key == 6:
+                self.search()
+            elif key == 19:
+                self.save_file()
+            elif key == 26:
+                self.undo()
+            elif key == curses.KEY_ENTER or key == 10:
+                self.new_line()
+            else:
+                self.addch(chr(key))
 
-	def addch(self, ch):
-		self.text[self.total_y] = self.text[self.total_y][:self.total_x] + ch + self.text[self.total_y][self.total_x:]
-		self.right()
+    def new_line(self):
+        try:
+            self.text.insert(self.total_y + 1, self.text[self.total_y][self.total_x:])
+            self.text[self.total_y] = self.text[self.total_y][:self.total_x]
+            self.down()
+        except Exception as e:
+            print(e)
 
-	def delch(self):
-		if self.total_x == 0:
-			self.text[self.total_y] += self.text[self.total_y + 1]
-			self.text.pop(self.total_y)
-		else:
-			self.text[self.total_y] = self.text[self.total_y][:self.total_x - 1] + self.text[self.total_y][self.total_x:]
-		self.left()
+    def search(self):
+        ...
 
-	def left(self):
-		if self.screen_x == 0 and self.total_y > 0:
-			self.screen_y -= 1
-			self.screen_x = len(self.text[self.total_y])
-		if self.screen_x > 0:
-			self.screen_x -= 1
+    def undo(self):
+        ...
 
-	def right(self):
-		if self.screen_x == len(self.text[self.total_y]) - 1:
-			self.screen_y += 1
-			self.screen_x = 0
-		elif self.screen_x < self.width - 1 and self.screen_x <= len(self.text[self.total_y]) - 1:
-			self.screen_x += 1
+    def addch(self, ch):
+        if len(self.text[self.total_y]) < 1:
+            self.text[self.total_y] = ch
+        self.text[self.total_y] = self.text[self.total_y][:self.total_x] + ch + self.text[self.total_y][self.total_x:]
+        self.right()
 
-	
-	def up(self):
-		if self.screen_y > 0:
-			if self.screen_x >= len(self.text[self.total_y]) - 1:
-				self.screen_x = len(self.text[self.total_y]) - 1
-			self.screen_y -= 1
-		else:
-			self.offscreen_y -= 1
-	
-	def down(self):
-		if self.screen_y == len(self.text) - self.offscreen_y - 1:
-			return
-		if self.screen_y < self.height - 1:
-			if self.screen_x >= len(self.text[self.total_y]) - 1:
-				self.screen_x = len(self.text[self.total_y + 1]) - 1
-			self.screen_y += 1
-		else:
-			self.offscreen_y += 1
+    def delch(self):
+        if self.total_x == 0:
+            self.text[self.total_y - 1] += self.text[self.total_y]
+            self.text.pop(self.total_y)
+        else:
+            self.text[self.total_y] = self.text[self.total_y][:self.total_x - 1] + self.text[self.total_y][self.total_x:]
+        self.left()
 
-	def save(self):
-		with open(self.file_name, 'w') as f:
-			for line in self.text:
-				f.write(line)
-			self.status = 'Saved as ' + self.file_name
 
+    def left(self):
+        if self.screen_x == 0:
+            if self.total_y == 0:
+                return
+            self.screen_y -= 1
+            self.screen_x = len(self.text[self.total_y - 1]) - 1
+        else:
+            self.screen_x -= 1
+
+    def right(self):
+        # if cursor is at end of line
+        if self.screen_x > len(self.text[self.total_y]) - 1:
+            if self.total_y == len(self.text) - 1: # if at the last line
+                return
+            self.screen_x = 0
+            self.screen_y += 1
+            return
+        elif self.screen_x >= self.width - 1:
+            self.offscreen_x += 1
+        elif self.screen_x < self.width - 1:
+            self.screen_x += 1
+    
+    def up(self):
+        if self.total_y == 0:
+            return
+        if self.screen_y > 0:
+            if self.screen_x >= len(self.text[self.total_y - 1]):
+                self.screen_x = len(self.text[self.total_y - 1])
+            self.screen_y -= 1
+        else:
+            self.offscreen_y -= 1
+    
+    def down(self):
+        if self.total_y == len(self.text) - 1:
+            return
+        if self.screen_y < self.height - 1:
+            if self.screen_x >= len(self.text[self.total_y + 1]):
+                self.screen_x = len(self.text[self.total_y + 1])
+            self.screen_y += 1
+        else:
+            self.offscreen_y += 1
+
+    def save_file(self):
+        if sys.platform == 'win32':
+            newline = '\r\n'
+        elif sys.platform == 'linux':
+            newline = '\n'
+        else:
+            newline = '\n'
+        with open(self.file_name, 'w') as f:
+            for line in self.text:
+                f.write(line + newline)
+            self.status = f'Saved as {self.file_name}'
 
 def c_main(win: curses.window):
-	file_name = sys.argv[1] if len(sys.argv) > 1 else None
-	editor = Editor(win, file_name)
-	editor.main()
+    file_name = sys.argv[1] if len(sys.argv) > 1 else None
+    editor = Editor(win, file_name)
+    editor.main()
 
-	if len(sys.argv) > 1:
-		file_name = sys.argv[1]
-	else: 
-		file_name = 'output'
-	if os.path.exists(file_name):
-		with open(file_name, 'r') as file:
-			text = file.readlines()
-	else:
-		with open(file_name, 'w') as file:
-			text = ''
-			
-# 	while RUN:
-# 		win.clear()
-		
-# 		for index, line in enumerate(text):
-# 			if wrap:
-# 				win.insstr(index, 0, line)
-# 			else:
-# 				win.insstr(index, 0, line)
-# 		win.move(cursor.y, cursor.x)
+    if len(sys.argv) > 1:
+        file_name = sys.argv[1]
+    else: 
+        file_name = 'output'
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as file:
+            text = file.readlines()
+    else:
+        with open(file_name, 'w') as file:
+            text = ''
 
 def main():
-	curses.wrapper(c_main)
+    curses.wrapper(c_main)
 
 if __name__ == '__main__':
-	exit(main())
+    exit(main())
